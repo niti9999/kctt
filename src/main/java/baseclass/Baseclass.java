b8b3208dd5a1ba97726cbd2e3f7e812da323843a
Nitish Kumar Singh <nitish.kumar70@gmail.com>
2020-09-19 12:58:53 +0530

    commit

--- src/main/java/baseclass/Baseclass.java
+++ src/main/java/baseclass/Baseclass.java
@@ -4,7 +4,7 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
 import static org.hamcrest.Matchers.not;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.concurrent.TimeUnit;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -19,7 +19,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Baseclass {
 
 	private WebDriver driver;
-	private String WaitForElementMaxDurationSecond;
+	int WaitForElementMaxDurationSecond;
 
 	/**
 	 * Main action class for the selenium
@@ -29,63 +29,45 @@ public class Baseclass {
 	 */
 	public void oActionsRequest(WebDriver driver, String jsonString) {
 
-		JSONObject jsonObject = (JSONObject) JSONValue.parse(jsonString);
+		JSONObject baseJsonObject = (JSONObject) JSONValue.parse(jsonString);
 
 		this.setDriver(driver);
-		String actionType = (String) jsonObject.get("ActionType");
-		String action = (String) jsonObject.get("Action");
-		String elementIdentifierType = (String) jsonObject.get("ElementIdentifierType");
-		String elementIdentifier = (String) jsonObject.get("ElementIdentifier");
-		String elementproperties = (String) jsonObject.get("Elementproperties");
-		String WaitForElementMaxDurationSecond = (String) jsonObject.get("WaitForElementMaxDurationSecond");
-
-		/*
-		 * ****************************************
-		 */
-		System.out.println("actionType: " + actionType);
-		System.out.println("action: " + action);
-		System.out.println("elementIdentifierType: " + elementIdentifierType);
-		System.out.println("elementIdentifier: " + elementIdentifier);
-		System.out.println("elementproperties: " + elementproperties);
-		System.out.println("WaitForElementMaxDurationSecond: " + WaitForElementMaxDurationSecond);
-		/*
-		 ******************************************* 
-		 */
+		String actionType = (String) baseJsonObject.get("ActionType");
+		String action = (String) baseJsonObject.get("Action");
+		String elementIdentifierType = (String) baseJsonObject.get("ElementIdentifierType");
+		String elementIdentifier = (String) baseJsonObject.get("ElementIdentifier");
+		String elementproperties = (String) baseJsonObject.get("Elementproperties");
+		WaitForElementMaxDurationSecond = ((Long) baseJsonObject.get("WaitForElementMaxDurationSecond")).intValue();
+		;
 
 		oActions(driver, actionType, action, elementIdentifierType, elementIdentifier, elementproperties);
+
+		LinkedHashMap<Integer, JSONObject> actionStepsMap = new LinkedHashMap<Integer, JSONObject>();
+		JSONArray actionStepsArray = (JSONArray) baseJsonObject.get("ActionSteps");
+
+		if (actionStepsArray != null) {
+			for (int i = 0; i < actionStepsArray.size(); i++) {
+
+				JSONObject actionStepJson = (JSONObject) JSONValue.parse(actionStepsArray.get(i).toString());
+				int StepNunber = ((Long) actionStepJson.get("StepNunber")).intValue();
+				JSONObject StepsJson = (JSONObject) actionStepJson.get("Steps");
+				actionStepsMap.put(StepNunber, StepsJson);
+			}
+		}
 
 		/*
 		 * get the array items from json
 		 */
-		ArrayList<String> listdata = new ArrayList<String>();
-		JSONArray arrayData = (JSONArray) jsonObject.get("ActionSteps");
-		if (arrayData != null) {
-			for (int i = 0; i < arrayData.size(); i++) {
-				listdata.add(arrayData.get(i).toString());
-				System.out.println(arrayData.get(i));
 
-				JSONObject jsonArrayObject = (JSONObject) JSONValue.parse(arrayData.get(i).toString());
+		if (actionStepsArray != null) {
+			for (int i = 1; i < actionStepsArray.size(); i++) {
+				actionStepsMap.get(i);
 
-				int aoStepNunber = ((Long) jsonArrayObject.get("StepNunber")).intValue();
-				String aoActionType = (String) jsonArrayObject.get("ActionType");
-				String aoAction = (String) jsonArrayObject.get("Action");
-				String aoElementIdentifierType = (String) jsonArrayObject.get("ElementIdentifierType");
-				String aoElementIdentifier = (String) jsonArrayObject.get("ElementIdentifier");
-				String aoElementproperties = (String) jsonArrayObject.get("Elementproperties");
-
-				/*
-				 * ****************************************
-				 */
-				System.out.println("aoStepNunber: " + aoStepNunber);
-				System.out.println("aoActionType: " + aoActionType);
-				System.out.println("aoAction: " + aoAction);
-				System.out.println("aoElementIdentifierType: " + aoElementIdentifierType);
-				System.out.println("aoElementIdentifier: " + aoElementIdentifier);
-				System.out.println("aoElementproperties: " + aoElementproperties);
-
-				/*
-				 ******************************************* 
-				 */
+				String aoActionType = (String) actionStepsMap.get(i).get("ActionType");
+				String aoAction = (String) actionStepsMap.get(i).get("Action");
+				String aoElementIdentifierType = (String) actionStepsMap.get(i).get("ElementIdentifierType");
+				String aoElementIdentifier = (String) actionStepsMap.get(i).get("ElementIdentifier");
+				String aoElementproperties = (String) actionStepsMap.get(i).get("ElementProperties");
 
 				oActions(driver, aoActionType, aoAction, aoElementIdentifierType, aoElementIdentifier,
 						aoElementproperties);
@@ -154,7 +136,7 @@ public class Baseclass {
 		 * WAIT
 		 */
 		case "wait":
-			WebDriverWait wait = new WebDriverWait(driver, Integer.parseInt(WaitForElementMaxDurationSecond));
+			WebDriverWait wait = new WebDriverWait(driver, WaitForElementMaxDurationSecond);
 			switch (action) {
 
 			case "waitUntilElementTobeVisible":
@@ -166,13 +148,12 @@ public class Baseclass {
 				break;
 
 			case "waitUntilTextToDisappear":
-				await().atMost(Integer.parseInt(WaitForElementMaxDurationSecond), SECONDS).pollInterval(1, SECONDS)
+				await().atMost((WaitForElementMaxDurationSecond), SECONDS).pollInterval(1, SECONDS)
 						.until(element::getText, not(elementproperties));
 				break;
 
 			case "implicitlyWait":
-				driver.manage().timeouts().implicitlyWait(Integer.parseInt(WaitForElementMaxDurationSecond),
-						TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait((WaitForElementMaxDurationSecond), TimeUnit.SECONDS);
 				break;
 			}
 		}
